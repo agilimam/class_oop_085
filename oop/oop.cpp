9eead0b2a4971bd64facb4a1c32e3243e4a08def
agilimam <agilimamalhafizi04@gmail.com>
2023-04-12 10:26:35 +0700

    Add project files.

---
+++ oop/oop.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+private:
+    int nim;
+    string nama;
+    float nilai;
+public:
+    void input() {
+        cout << "Masukkan nim : ";
+        cin >> nim;
+        cout << "Masukkan nama : ";
+        cin >> nama;
+        cout << "Masukkan nilai : ";
+        cin >> nilai;
+    }
+    void printData();
+};
+
+void Mahasiswa::printData() {
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
